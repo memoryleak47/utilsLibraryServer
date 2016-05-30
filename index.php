2a864a920fc358a8392d22ee63388bf2d523b55c
memoryleak47 <memoryleak47@protonmail.ch>
2016-05-30 21:42:13 +0200

    worked farther on getFiles

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	if (! isset($_GET['cmd'])) exit;
 	if ($_GET['cmd'] == "getFiles" && isset($_GET['conf']))
 	{
-		foreach (glob(dirname(__FILE__)."/confs/".$_GET['conf']."/*") as $file)
+		foreach (glob("confs/".$_GET['conf']."/*") as $file)
 		{
 			echo "$file \r\n";
 		}
