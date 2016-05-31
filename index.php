726dbd896c79ebd1fc55aea8cafc647219476e4a
memoryleak47 <memoryleak47@protonmail.ch>
2016-05-31 13:26:35 +0200

    added addUser

--- index.php
+++ index.php
@@ -11,11 +11,16 @@
 	mysql_select_db($db);
 
 	if (! isset($_GET['cmd'])) exit;
+
 	if ($_GET['cmd'] == "getFiles" && isset($_GET['conf']))
 	{
 		foreach (glob("confs/".$_GET['conf']."/*") as $file)
 		{
 			echo "$file ";
 		}
+	}
+	else if ($_GET['cmd'] == "addUser" && isset($_GET['username'] && isset($_GET['password'])))
+	{
+		mysql_query("INSERT INTO ulUsers (username, password) VALUES($_GET['username'], $_GET['password']");
 	}
 ?>
